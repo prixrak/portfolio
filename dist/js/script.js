deb059c3e8536f004eb68b8ef248f22b653402dc
prixrak <20arman15@gmail.com>
2021-09-06 12:26:50 +0300

    Create 4 section about skills.

--- dist/js/script.js
+++ dist/js/script.js
@@ -14,3 +14,10 @@ closeElem.addEventListener('click', () => {
 closeOverlaySide.addEventListener('click', () => {
     menu.classList.remove('menu_active');
 });
+
+const counters = document.querySelectorAll('.skills__ratings-counter'),
+        lines = document.querySelectorAll('.skills__ratings-line span');
+
+counters.forEach((item, i) => {
+    lines[i].style.width = item.innerHTML;
+});
