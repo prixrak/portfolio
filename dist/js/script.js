aa4f84c29b83cf9f4e1a190972bb58d02694253b
prixrak <20arman15@gmail.com>
2021-09-07 10:25:41 +0300

    Added local  hrefs to links

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,7 +1,8 @@
 const hamburger = document.querySelector('.hamburger'),
       menu = document.querySelector('.menu'),
       closeElem = document.querySelector('.menu__close'),
-      closeOverlaySide = document.querySelector('.menu__overlay');
+      closeOverlaySide = document.querySelector('.menu__overlay'),
+      linksToSections = document.querySelectorAll('.menu__link');
 
 hamburger.addEventListener('click', () => {
     menu.classList.add('menu_active');
@@ -13,6 +14,12 @@ closeElem.addEventListener('click', () => {
 
 closeOverlaySide.addEventListener('click', () => {
     menu.classList.remove('menu_active');
+});
+
+linksToSections.forEach(item => {
+    item.addEventListener('click', () => {
+        menu.classList.remove('menu_active');
+    });
 });
 
 const counters = document.querySelectorAll('.skills__ratings-counter'),
